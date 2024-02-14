c1f43091b7416362322e1175388aa225451c92a1
Allan Kiprop <kipropa77@gmail.com>
2024-02-14 14:31:25 +0000

    rect

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,5 +3,13 @@
 
 
 class Rectangle:
-    """Rectangle class to represent a rectangle"""
-    pass
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
