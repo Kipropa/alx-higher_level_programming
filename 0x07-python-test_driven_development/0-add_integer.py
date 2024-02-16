0571ad44a30242959a4733273e1256f4b505f91d
Allan Kiprop <kipropa77@gmail.com>
2024-02-16 11:43:01 +0000

    a function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-
-"""Matrix divisor function"""
+"""
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(). For example,
+>>> add_integer(10, 12)
+"""
 
 
 def add_integer(a, b=98):
     """
-    Adds two integers.
-
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number. Defaults to 98.
-
-    Returns:
-        int: The sum of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+        Adds two integer.
+        Args:
+            a (int|float): An integer or floating point number
+            b (int|float): An integer or floating point number
+        Returns: The sum of `a` and `b`
     """
-    # Check if a is an integer or float
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-    
-    # Check if b is an integer or float
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
 
-    # Convert a and b to integers if they are floats
-    a = int(a)
-    b = int(b)
+    if type(a) is float:
+        a = int(a)
 
-    # Perform addition and return the result
-    return a + b
+    if type(b) is float:
+        b = int(b)
+
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return (a + b)
 
