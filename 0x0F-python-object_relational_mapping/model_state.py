8fffcfd85fe595745e5a18a5986c0a9faf334286
Allan Kiprop <kipropa77@gmail.com>
2024-05-01 16:02:47 +0000

    lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
 """
-Defines a state model that contain the class definition
- of a City and an instance Base = declarative_base()
+Defines the state model, containing the State class and
 """
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
-from sqlalchemy import Column, ForeignKey, Integer, String, null
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Integer, Column, String
+from sqlalchemy.orm import declarative_base
 
+engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa')
 Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     """
-    inherits from Base (imported from model_state)
-    links to the MySQL table cities
-
-    class attribute id that represents a column of
-    an auto-generated, unique integer, can't be null and is a primary key
-
-    class attribute name that represents a column
-    of a string of 128 characters and can't be null
-
-    class attribute state_id that represents a column
-    of an integer, can't be null and is a foreign key to states.id
-
+    State class
+        id -> state.id
+        name -> state.name
     """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    def __repr__(self):
+        return "{}: {}".format(self.id, self.name)
